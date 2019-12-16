e3f6e7e672eab9e4f18aebae1d94128af107eaf3
haoyang yu <yhy@yhydeMacBook-Pro.local>
2019-12-16 16:13:27 +0800

    test

--- ZzbGameSDK/Classes/DemoViewController.m
+++ ZzbGameSDK/Classes/DemoViewController.m
@@ -7,72 +7,14 @@
 //
 
 #import "DemoViewController.h"
-#import "DemoPlugin.h"
-#import "TestView.h"
-#import <objc/runtime.h>
+
 
 @interface DemoViewController ()
 
 @end
 
 @implementation DemoViewController {
-    IMP _oldImp;
-//    BOOL _closed;
-}
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        [self registerNativeView:TestView.class forType:@"test"];
-    }
-    return self;
 }
-
-- (void)initializePlugins {
-    [super initializePlugins];
-    
-    [self registerPlugin:[DemoPlugin new]];
-}
-
-//- (void)viewDidLoad {
-//    [super viewDidLoad];
-//    Class clz = [UIApplication.sharedApplication.delegate class];
-//    Method method = class_getInstanceMethod(clz, @selector(application:supportedInterfaceOrientationsForWindow:));
-//    if (method) {
-//        _oldImp = method_getImplementation(method);
-//    }else {
-//        struct objc_method_description m = protocol_getMethodDescription(@protocol(UIApplicationDelegate),
-//                                      @selector(application:supportedInterfaceOrientationsForWindow:),
-//                                      NO, YES);
-//        _oldImp = imp_implementationWithBlock(^(id del, id app, id window) {
-//            return UIInterfaceOrientationMaskPortrait;
-//        });
-//        class_addMethod(clz, @selector(application:supportedInterfaceOrientationsForWindow:), _oldImp, m.types);
-//        method = class_getInstanceMethod(clz, @selector(application:supportedInterfaceOrientationsForWindow:));
-//    }
-//    method_setImplementation(method, imp_implementationWithBlock(^(id del, id app, id window){
-//        return UIInterfaceOrientationMaskPortrait | UIInterfaceOrientationMaskLandscape;
-//    }));
-//}
-//
-//typedef UIInterfaceOrientationMask (*tIMP)(id obj, SEL sel, id app, id window);
-//
-//- (void)close {
-//    Class clz = [UIApplication.sharedApplication.delegate class];
-//    Method method = class_getInstanceMethod(clz, @selector(application:supportedInterfaceOrientationsForWindow:));
-//
-//    UIInterfaceOrientationMask mask = ((tIMP)_oldImp)(UIApplication.sharedApplication.delegate, @selector(application:supportedInterfaceOrientationsForWindow:), nil, nil);
-//    if (mask == UIInterfaceOrientationMaskPortrait) {
-//        [[UIDevice currentDevice] setValue:@(UIInterfaceOrientationPortrait) forKey:@"orientation"];
-//        [UIViewController attemptRotationToDeviceOrientation];
-//    } else if (mask == UIInterfaceOrientationMaskLandscape) {
-//        [[UIDevice currentDevice] setValue:@(UIInterfaceOrientationLandscapeRight) forKey:@"orientation"];
-//        [UIViewController attemptRotationToDeviceOrientation];
-//    }
-//    [UIViewController attemptRotationToDeviceOrientation];
-//    [super close];
-//
-//    method_setImplementation(method, _oldImp);
-//}
 
 @end
